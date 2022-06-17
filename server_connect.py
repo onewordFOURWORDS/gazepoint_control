a522c3df730e4e71dcd1c6276efc4370ad5387df
onewordFOURWORDS <timdanham@gmail.com>
2022-06-17 09:55:00 -0400

    working server conection

---
+++ server_connect.py
@@ -0,0 +1,36 @@
+import socket
+import numpy as np
+
+# Host machine IP/IP address of Windows VM
+host = "192.168.56.101"
+# Gazepoint Port
+port = 4242
+address = (host, port)
+# Connect to Gazepoint control server on Windows VM
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(address)
+
+# Send command to request certain data
+s.send(str.encode('<SET ID="ENABLE_SEND_BLINK" STATE="1" />\r\n'))
+s.send(str.encode('<SET ID="ENABLE_SEND_POG_BEST" STATE="1" />\r\n'))
+# Enable the server to return the data requested
+s.send(str.encode('<SET ID="ENABLE_SEND_DATA" STATE="1" />\r\n'))
+
+for i in range(5):
+        s.recv(1024)
+while 1:
+    rxdat = s.recv(1024)  # Receive data from server
+    rxdat = bytes.decode(rxdat)
+
+    # Split each data point into a separate item
+    rxdat = rxdat.split()
+    # Remove the first and last item in the list
+    # Removes '<REC' and '/>' items
+    rxdat = rxdat[1:-1]
+
+    # Convert to numpy array
+    rxdat = np.asarray(rxdat)
+    # make array vertical + converts into 2d array
+    rxdat = rxdat.reshape(-1, 1)
+
+    print(rxdat)
